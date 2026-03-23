7e475f3cb26122fc873c89a1c0571731059a27da
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-23 12:27:45 +0200

    feat: prettier formating

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 import { useRouter } from "next/navigation";
 import useStore from "../store/store";
 
@@ -9,22 +9,27 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <h1>
-          Welcome Interviewer!
-        </h1>
+        <h1>Welcome Interviewer!</h1>
         <div>
           <article>Please search a user</article>
         </div>
         <div>
-          <input type="text" placeholder="Enter User name" value={Search} onChange={(e) => setSearch(e.target.value)}/>
-          <button onClick={() => {
-            if(Search){
-              router.push(`/demo/${Search}`)
-            }else{
-              //for now
-              alert("no user name was entered")
-            }
-          }}>
+          <input
+            type="text"
+            placeholder="Enter User name"
+            value={Search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <button
+            onClick={() => {
+              if (Search) {
+                router.push(`/demo/${Search}`);
+              } else {
+                //for now
+                alert("no user name was entered");
+              }
+            }}
+          >
             Search
           </button>
         </div>
@@ -32,4 +37,3 @@ export default function Home() {
     </div>
   );
 }
-
