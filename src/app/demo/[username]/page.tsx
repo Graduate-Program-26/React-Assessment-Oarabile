facb34d297651170b58a37c010ebefb77c8b0543
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-23 21:17:17 +0200

    prettier and more page content

--- src/app/demo/[username]/page.tsx
+++ src/app/demo/[username]/page.tsx
@@ -1,41 +1,50 @@
-import { githubEvents, githubRepo, githubUser } from "@/src/lib/github"
+import { githubEvents, githubRepo, githubUser } from "@/src/lib/github";
+import { UserRepo, User, Event } from "@/src/types/types";
+import { notFound } from "next/navigation";
 
-export default async function Page({params}: {params : Promise<{username: string}>}){
-    const {username} = await params;
-    const gitUser = await githubUser(username);
-    const gitRepo = await githubRepo(username);
-    const gitEvents = await githubEvents(username);
-    return(
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ username: string }>;
+}) {
+  let gitUser: User;
+  let gitRepo: UserRepo[];
+  let gitEvents: Event[];
+  
+
+  try {
+    const { username } = await params;
+    gitUser = await githubUser(username);
+    gitRepo = await githubRepo(username);
+    gitEvents = await githubEvents(username);
+
+    return (
+      <div>
+        <div>{gitUser.login}</div>
+        <img src={gitUser.avatar_url} alt="profile picture" />
         <div>
-            <div>{gitUser.login}</div>
-            <img src={gitUser.avatar_url} alt="profile picture" />
-            <div>
-                {gitRepo.map((item) => (
-                    <div key={item.id}>
-                        <h3>{item.name}</h3>
-                        <p>{ (item.description) ? (item.description): "No description"}</p>
-                        <div>
-                            {item.stargazers_count}
-                        </div>
-                        <div>
-                            {item.language}
-                        </div>
-                        <div>
-                            {item.updated_at}
-                        </div>
-                    </div>
-                ))}
+          {gitRepo.map((item) => (
+            <div key={item.id}>
+              <h3>{item.name}</h3>
+              <p>{item.description ? item.description : "No description"}</p>
+              <div>{item.stargazers_count}</div>
+              <div>{item.language}</div>
+              <div>{item.updated_at}</div>
             </div>
-            <div>
-                {gitEvents.map((item)=>(
-                    <div>
-                        <div>{item.type}</div>
-                        <div>{item.repo.name}</div>
-                        <div>{item.created_at}</div>
-                    </div>
-                ))}
-            </div>
-
+          ))}
         </div>
-    );    
+        <div>
+          {gitEvents.map((item) => (
+            <div key={item.repo.name}>
+              <div>{item.type}</div>
+              <div>{item.repo.name}</div>
+              <div>{item.created_at}</div>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  } catch {
+    notFound();
+  }
 }
