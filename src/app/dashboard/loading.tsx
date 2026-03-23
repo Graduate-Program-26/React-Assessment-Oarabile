db9680607f8665135a4a03cc4f100fba0ee88c61
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-23 22:59:08 +0200

    feat: added loading pages, error pages in both directories

---
+++ src/app/dashboard/loading.tsx
@@ -0,0 +1,9 @@
+export default function loading(){
+    //havent added daisy yet
+    return(
+        <div>
+            <span className="loading loading-spinner loading-xl"></span>
+            Loading
+        </div>
+    )
+}
