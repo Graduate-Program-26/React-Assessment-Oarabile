e681d5525e86f413e20b522d0e2c55ea5e4d93c6
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-24 22:17:08 +0200

    feat: added not found page styling

--- src/app/profile/[username]/not-found.tsx
+++ src/app/profile/[username]/not-found.tsx
@@ -1,9 +1,16 @@
-
-export default async function not_Found(){
-    return(
-        <div>
-            <img src="/Thinking face-rafiki.svg" alt="page has not been found"/>
-            <h2>user not found</h2>
-        </div>
-    );
+export default async function NotFound() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div>
+        <img
+          src="/Thinking face-rafiki.svg"
+          className="w-64 md:w-96 lg:w-[500px]"
+          alt="page has not been found"
+        />
+      </div>
+      <div>
+        <h2 className="text-2xl font-semibold mt-4">User Not Found</h2>
+      </div>
+    </div>
+  );
 }
