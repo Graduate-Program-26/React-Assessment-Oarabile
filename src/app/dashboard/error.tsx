5cbee8d0086b318b982cb5f7d88ab13c8758c389
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-24 23:07:31 +0200

    feat: added dashboard styling

--- src/app/dashboard/error.tsx
+++ src/app/dashboard/error.tsx
@@ -9,22 +9,30 @@ export default function ErrorPage({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
-
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by re-fetching and re-rendering the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div>
+        <img
+          src="/Thinking face-rafiki.svg"
+          className="w-64 md:w-96 lg:w-[500px]"
+          alt="page has not been found"
+        />
+      </div>
+      <div>
+        <h2>Something went wrong!</h2>
+        <button
+          onClick={
+            // Attempt to recover by re-fetching and re-rendering the segment
+            () => reset()
+          }
+        >
+          Try again
+        </button>
+      </div>
     </div>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   const setSearch = useStore((state) => state.setSearchInput);
   const Search = useStore((state) => state.searchInput);
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <div className="">
+      <main className="">
         <h1>Welcome Interviewer!</h1>
         <div>
           <article>Please search a user</article>
