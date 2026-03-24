7326c4f75f1a2b34bbf90f0d8caf7e3a58c4e088
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-24 23:39:33 +0200

    feat: added landing page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,29 +9,40 @@ export default function Home() {
   return (
     <div className="">
       <main className="">
-        <h1>Welcome Interviewer!</h1>
-        <div>
-          <article>Please search a user</article>
-        </div>
-        <div>
-          <input
-            type="text"
-            placeholder="Enter User name"
-            value={Search}
-            onChange={(e) => setSearch(e.target.value)}
-          />
-          <button
-            onClick={() => {
-              if (Search) {
-                router.push(`/profile/${Search}`);
-              } else {
-                //for now
-                alert("no user name was entered");
-              }
-            }}
-          >
-            Search
-          </button>
+        <div className="flex items-center justify-center mt-[5%]">
+          <div>
+            <div className="text-center lg:text-left">
+              <h1 className="text-5xl font-bold text-[#90CAF9]">Welcome Interviewer!</h1>
+              <p className="py-6 text-xl opacity-80">Search for a GitHub user to view their professional <br />profile and repository stats.</p>
+            </div>
+            <div className="flex">
+              <input
+                type="text"
+                placeholder="Enter User name"
+                value={Search}
+                className="input input-bordered focus:outline-[#90CAF9]"
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button
+                onClick={() => {
+                  if (Search) {
+                    router.push(`/profile/${Search}`);
+                  } else {
+                    //for now
+                    alert("no user name was entered");
+                  }
+                }}
+                className="btn btn-primary bg-[#90CAF9] border-[#90CAF9] hover:bg-[#74b9ef] hover:border-[#74b9ef] ml-[2%]"
+              >
+                Search
+              </button>
+            </div>
+          </div>
+          
+
+          <div>
+            <img src="/Online resume-cuate.svg" className="w-64 md:w-96 lg:w-[600px]" alt="" />
+          </div>
         </div>
       </main>
     </div>
