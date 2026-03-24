37c4ea96ca83804b22ef8ae7905e94736d65b02d
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-24 18:02:23 +0200

    feat: added svg image

--- src/app/profile/[username]/not-found.tsx
+++ src/app/profile/[username]/not-found.tsx
@@ -2,7 +2,7 @@
 export default async function not_Found(){
     return(
         <div>
-            <img src="" alt="page has not been found"/>
+            <img src="/Thinking face-rafiki.svg" alt="page has not been found"/>
             <h2>user not found</h2>
         </div>
     );
