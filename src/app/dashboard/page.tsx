c38334cfe54d575bfaa10a61f53aa96ba22cf28d
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-25 15:29:41 +0200

    feat: added stling to profile page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { auth } from "@/src/lib/auth"
 import { githubEvents, githubRepo, githubUser } from "@/src/lib/github";
 import { notFound } from "next/navigation";
 import { User, Event, UserRepo } from "@/src/types/types";
+import Card, { AvatarImage, MiniCards } from "@/src/components/layout/card";
+import Stats from "@/src/components/layout/stats";
 
 export default async function Page(){
 
@@ -20,33 +22,33 @@ export default async function Page(){
 
     return(
         <main>
-            <div>
-                <div>{userData.login}</div>
-                <img src={userData.avatar_url} alt="profile picture"/>
-                <div>
-                    {userRepo.map((item) => (
-                        <div key={item.id}>
-                            <h3>{item.name}</h3>
-                            <p>{item.description ? item.description : "No description"}</p>
-                            <div>{item.stargazers_count}</div>
-                            <div>{item.language}</div>
-                            <div>{item.updated_at}</div>
-                        </div>
-                    ))}
-                </div>
-                <div>
-                    {userEvents.map((item) => (
-                        <div key={item.id}>
-                            <div>{item.type}</div>
-                            <div>{item.repo.name}</div>
-                            <div>{item.created_at}</div>
-                        </div>
-                    ))}
-                </div>
-                <div>
-                    <img src={`https://ghchart.rshah.org/${userData.login}`} alt="GitHub contribution calendar" />
-                </div>
+            <div className="flex justify-center items-center">
+            <AvatarImage image={userData.avatar_url} />
+            <div className="ml-[3%]">
+                <h1 className="text-5xl font-extrabold tracking-tight text-base-content mb-[4%]">{userData.login}</h1>
+                <Stats data={userData}/>
             </div>
+            </div>
+            <div className="flex justify-center">
+            <div className="mr-[2%]">
+                <Card data={userEvents}/>
+            </div>
+            <div>
+                <h3 className="text-2xl font-extrabold">Repositories</h3>
+                <MiniCards data={userRepo}/>
+            </div>
+            </div>
+            <div className="mb-[2%]">
+            <h3 className=" ml-[18%] text-3xl font-extrabold m-[1.8%]">Chart</h3>
+            <div className="flex justify-center">
+                <img 
+                src={`https://ghchart.rshah.org/90CAF9/${userData.login}`} 
+                alt="GitHub contribution calendar"
+                className="w-5xl " 
+                />
+            </div>
+        </div>
+      
         </main>
     )
 }
