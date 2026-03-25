75af1a1e3b8a1288b75ae5b4f8904bb799730f41
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-25 13:03:50 +0200

    feat: loading screen

--- src/app/dashboard/loading.tsx
+++ src/app/dashboard/loading.tsx
@@ -1,8 +1,7 @@
 export default function loading(){
     return(
-        <div className="flex justify-center items-center">
+        <div className="flex flex-col items-center mt-[20%]">
             <span className="loading loading-spinner loading-xl"></span>
-            Loading
         </div>
     );
 }
