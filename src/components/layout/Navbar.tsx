d3f44b95a48d216be377d07a82fa3237e9390a31
Oarabile Mbewe <ombewe@dvtsoftware.com>
2026-03-26 10:28:45 +0200

    feat: added theme toggle

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -46,7 +46,13 @@ export default async function Navbar() {
                 <a className="btn btn-ghost text-xl ml-[-15] font-bold">GitDash</a>
             </div>
             <div className="navbar-end">
-                <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto mr-[2%]" />
+                <label className="toggle text-base-content mr-[3%]">
+                    <input type="checkbox" value="synthwave" className="theme-controller" />
+
+                    <svg aria-label="sun" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><circle cx="12" cy="12" r="4"></circle><path d="M12 2v2"></path><path d="M12 20v2"></path><path d="m4.93 4.93 1.41 1.41"></path><path d="m17.66 17.66 1.41 1.41"></path><path d="M2 12h2"></path><path d="M20 12h2"></path><path d="m6.34 17.66-1.41 1.41"></path><path d="m19.07 4.93-1.41 1.41"></path></g></svg>
+
+                    <svg aria-label="moon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path></g></svg>
+                </label>
                 <div className="dropdown dropdown-end">
                     <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full">
